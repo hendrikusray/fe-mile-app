3fb2d6b94e02eb4843878b2ae5f765eb5bca0954
Hendrikus Adi Purnama <Hendri@NBMC24050026.local>
2025-10-26 20:42:53 +0700

    enhance

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,5 +1,9 @@
-export default {
-  content: ["./index.html","./src/**/*.{vue,js}"],
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./index.html",
+    "./src/**/*.{vue,js,ts,jsx,tsx}",
+  ],
   theme: { extend: {} },
   plugins: [],
 }
